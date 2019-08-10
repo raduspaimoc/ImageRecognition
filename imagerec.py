31d17bae058cd5c152d2efd9d74e3c8e16e5f928
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-10 18:56:56 +0200

    Threshold function to convert images in black/white implemented

---
+++ imagerec.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import PIL
+from PIL import Image
+
+# def image_copy(image_array):
+#     image_copy = np.copy(image_array)
+#     vis_util.visualize_boxes_and_labels_on_image_array(
+#         image_copy,
+#         np.squeeze(boxes),
+#         np.squeeze(classes).astype(np.int32),
+#         np.squeeze(scores),
+#         category_index,
+#         use_normalized_coordinates=True,
+#         line_thickness=8)
+#     return image_copy
+
+
+def threshold(imageArray):
+
+    balanceAr = []
+    newAr = imageArray
+    from statistics import mean
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = mean(eachPix[:3])
+            balanceAr.append(avgNum)
+
+    balance = mean(balanceAr)
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if mean(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+if __name__ == '__main__':
+
+    i = Image.open('images/numbers/0.1.png')
+    iar = np.array(i)
+    i2 = Image.open('images/numbers/y0.4.png')
+    iar2 = np.array(i2)
+    i3 = Image.open('images/numbers/y0.5.png')
+    iar3 = np.array(i3)
+    i4 = Image.open('images/sentdex.png')
+    iar4 = np.array(i4)
+
+
+    iar = threshold(iar)
+    iar2 = threshold(iar2)
+    iar3 = threshold(iar3)
+    iar4 = threshold(iar4)
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
+    ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
+    ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
+    ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
+
+    ax1.imshow(iar)
+    ax2.imshow(iar2)
+    ax3.imshow(iar3)
+    ax4.imshow(iar4)
+
+
+    plt.show()
