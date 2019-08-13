8b7e479b6d20b5e93aeb514244ba9e786d78209f
rspaimoc2 <raduspaimoc@gmail.com>
2019-08-13 21:53:46 +0200

    [WIP] Resize image

--- imagerec.py
+++ imagerec.py
@@ -4,6 +4,8 @@ import time
 import PIL
 from PIL import Image
 from collections import Counter
+from resizeimage import resizeimage
+
 
 # def image_copy(image_array):
 #     image_copy = np.copy(image_array)
@@ -25,6 +27,7 @@ def whatNumIsThis(filepath):
 
     i = Image.open(filepath)
     iar = np.array(i)
+    iar = threshold(iar)
     iarl = iar.tolist()
 
     inQuestion = str(iarl)
@@ -47,6 +50,24 @@ def whatNumIsThis(filepath):
     x = Counter(matchedAr)
     print x
 
+    graphX = []
+    graphY = []
+    for eachThing in x:
+        print eachThing
+        graphX.append(eachThing)
+        print x[eachThing]
+        graphY.append(x[eachThing])
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((4,4), (0,0), rowspan=1, colspan=4)
+    ax2 = plt.subplot2grid((4,4), (1,0), rowspan=3, colspan=4)
+
+    ax1.imshow(iar)
+    ax2.bar(graphX, graphY, align='center')
+    plt.ylim(300)
+    xloc = plt.MaxNLocator(12)
+    ax2.xaxis.set_major_locator(xloc)
+    plt.show()
 
 def createExamples():
     numberArrayExamples = open('numArEx.txt', 'a')
@@ -70,24 +91,41 @@ def threshold(imageArray):
     from statistics import mean
     for eachRow in imageArray:
         for eachPix in eachRow:
-            avgNum = mean(eachPix[:3])
+            avgNum = mean(eachPix[:len(eachPix)-1])
             balanceAr.append(avgNum)
 
     balance = mean(balanceAr)
     for eachRow in newAr:
         for eachPix in eachRow:
-            if mean(eachPix[:3]) > balance:
-                eachPix[0] = 255
-                eachPix[1] = 255
-                eachPix[2] = 255
-                eachPix[3] = 255
+            if mean(eachPix[:len(eachPix)-1]) > balance:
+                for i in range(0, len(eachPix)):
+                    eachPix[i] = 255
+
+                #eachPix[0] = 255
+                #eachPix[1] = 255
+                #eachPix[2] = 255
+                #eachPix[3] = 255
             else:
-                eachPix[0] = 0
-                eachPix[1] = 0
-                eachPix[2] = 0
-                eachPix[3] = 255
+                for i in range(0, len(eachPix) - 1):
+                    if i == len(eachPix):
+                        eachPix[i] = 255
+                    else:
+                        eachPix[i] = 0
+                # eachPix[0] = 0
+                # eachPix[1] = 0
+                # eachPix[2] = 0
+                # eachPix[3] = 255
     return newAr
 
 if __name__ == '__main__':
 
-    whatNumIsThis('images/test2.png')
+    #whatNumIsThis('test.png')
+
+    # Resize image to 8x8px
+    fd_img = open('images-to-resize/numred3.jpg')
+    img = Image.open(fd_img)
+    img = resizeimage.resize_thumbnail(img, [8, 8], resample=Image.LANCZOS)
+    #img = resizeimage.resize_cover(img, [8, 8])
+    img.save('test.png', 'png')
+    fd_img.close()
+    whatNumIsThis('test.png')
